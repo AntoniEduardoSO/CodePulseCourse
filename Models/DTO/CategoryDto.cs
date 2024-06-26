8a48b7e002b0cbaaa800d637d5dfac99621011e0
Antônio Eduardo Souza de Oliveira <antoniooliveira@semed.maceio.al.gov.br>
2024-06-26 13:54:47 -0300

    Post categories

---
+++ Models/DTO/CategoryDto.cs
@@ -0,0 +1,10 @@
+namespace CodePulse.API.Models.DTO
+{
+    public class CategoryDto
+    {
+        public Guid Id {get;set;}
+        public required string Name {get;set;}
+        public required string UrlHandle {get;set;}
+    }    
+}
+
